a54dfeda36b20e39706b206a676dbbd234c08b48
joelemerson <joelnann@yahoo.com>
2016-10-20 09:23:14 -0500

    Fixed sale button to decrement the appropriate list item quantity.

--- app/src/main/java/com/example/android/items/ItemCursorAdapter.java
+++ app/src/main/java/com/example/android/items/ItemCursorAdapter.java
@@ -31,10 +31,6 @@ import android.widget.TextView;
 import com.example.android.items.data.ItemContract;
 import com.example.android.items.data.ItemContract.ItemEntry;
 
-import java.text.NumberFormat;
-
-import static android.R.attr.id;
-
 /**
  * {@link ItemCursorAdapter} is an adapter for a list or grid view
  * that uses a {@link Cursor} of items data as its data source. This adapter knows
@@ -79,58 +75,49 @@ public class ItemCursorAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, final Context context, final Cursor cursor) {
-
         final int itemId = cursor.getInt(cursor.getColumnIndexOrThrow(ItemEntry._ID));
         final int itemQty = cursor.getInt(cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_QUANTITY));
-
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
         TextView descriptionTextView = (TextView) view.findViewById(R.id.description);
         TextView priceTextView = (TextView) view.findViewById(R.id.price);
         TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
-
         // Find the columns of items attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_NAME);
         int descriptionColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_DESCRIPTION);
         int priceColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_PRICE);
         int quantityColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_QUANTITY);
-
         // Read the item attributes from the Cursor for the current item
         String itemName = cursor.getString(nameColumnIndex);
         String itemDescription = cursor.getString(descriptionColumnIndex);
         String itemPrice = cursor.getString(priceColumnIndex);
         String itemQuantity = cursor.getString(quantityColumnIndex);
-
         // If the item description is empty string or null, then use some default text
         // that says "Unknown Description", so the TextView isn't blank.
         if (TextUtils.isEmpty(itemDescription)) {
             itemDescription = context.getString(R.string.unknown_description);
         }
-
         // Update the TextViews with the attributes for the current item
         nameTextView.setText(itemName);
         descriptionTextView.setText(itemDescription);
         priceTextView.setText(itemPrice);
         quantityTextView.setText(itemQuantity);
-
         //Process a sale when the button is clicked and subtract one from the current inventory
-
         Button saleButton = (Button) view.findViewById(R.id.salebutton);
         saleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                ContentValues values=new ContentValues();
-
+                ContentValues values = new ContentValues();
                 if (itemQty > 0) {
-                    //itemQty--;
-                    values.put(ItemContract.ItemEntry.COLUMN_ITEM_QUANTITY, itemQty);
+                    int mItemQty;
+                    mItemQty = (itemQty - 1);
+                    values.put(ItemContract.ItemEntry.COLUMN_ITEM_QUANTITY, mItemQty);
                     Uri uri = ContentUris.withAppendedId(ItemContract.ItemEntry.CONTENT_URI, itemId);
                     context.getContentResolver().update(uri, values, null, null);
                 }
-
                 context.getContentResolver().notifyChange(ItemContract.ItemEntry.CONTENT_URI, null);
             }
-        });}
+        });
     }
+}
 
